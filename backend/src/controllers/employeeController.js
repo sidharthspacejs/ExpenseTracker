fe903195fa20f8d2e7c8f0018f13c73e0594950b
sidharthspacejs <sidharthjs57@gmail.com>
2026-06-23 22:37:57 +0530

    View expenses added and zod for validation

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,15 +1,19 @@
 import prisma from '../config/prisma.js'
+import { expenseSchema } from '../../validations/expenseValidations.js';
 
 
 export const createExpense = async(req,res) => {
 
-    const {amount, description, category} = req.body;
+    const result = expenseSchema.safeParse(req.body);  //Input Validation
 
-    if(amount === undefined || !description || !category) {
+    if(!result.success) {
         return res.status(400).json({
-            message: "All fields are required"
+            errors: result.error.issues
         });
     }
+
+    const {amount, description, category} = result.data; 
+
 
     try {
         
@@ -34,18 +38,18 @@ export const createExpense = async(req,res) => {
         return res.status(201).json({
             message: "Expense created successfully",
             expense
-        })
+        });
     } catch (error) {
 
         console.log(error);
 
         return res.status(500).json({
             message: "Internal Server Error"
-        })
+        });
     }
 }
 
-export const viewAllExpense = async(req,res) => {
+export const viewMyExpense = async(req,res) => {
 
     try {
         const expenses = await prisma.expense.findMany({
@@ -60,7 +64,7 @@ export const viewAllExpense = async(req,res) => {
                     }
                 }
             }
-        })
+        });
 
         return res.status(200).json({
             expenses
@@ -71,6 +75,67 @@ export const viewAllExpense = async(req,res) => {
 
         return res.status(500).json({
             message: "Internal Server Error"
-        })
+        });
     }
+}
+
+
+export const updateExpense = async(req,res) => {
+
+    const id = Number(req.params.id);
+
+    const result = expenseSchema.safeParse(req.body);
+
+    if(!result.success) {
+        return res.status(400).json({
+            errors: result.error.issues
+        });
+    }
+
+    const {amount, description, category} = result.data;
+
+    try {
+        
+        const expense = await prisma.expense.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if(!expense) {
+            return res.status(404).json({
+                message: "Expense not found"
+            });
+        }
+
+        if(expense.userId !== req.user.id) {
+            return res.status(403).json({
+                message: "Access Denied"
+            });
+        }
+
+        const updated = await prisma.expense.update({
+            where: {
+                id
+            },
+            data: {
+                amount,
+                description,
+                category
+            }
+        });
+
+        return res.status(200).json({
+            message: "Expense updated successfully",
+            updated
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+
+
 }
--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {createExpense} from '../controllers/employeeController.js'
 import {authenticate} from "../middlewares/authMiddleware.js"
 import { isEmployee } from "../middlewares/employeeMiddleware.js"
-import { viewAllExpense } from '../controllers/employeeController.js';
+import { viewMyExpense } from '../controllers/employeeController.js';
+import { updateExpense } from '../controllers/employeeController.js';
 
 const router = express.Router();
 
 router.post('/create-expense',authenticate,isEmployee,createExpense);
-router.get('/view-expenses',authenticate,isEmployee,viewAllExpense);
+router.get('/view-expenses',authenticate,isEmployee,viewMyExpense);
+router.put('/expense/:id',authenticate,isEmployee,updateExpense);
 
 export default router;
