21fd69480f7fcd5acedcdb47e1bdd219a7558588
sidharthspacejs <sidharthjs57@gmail.com>
2026-06-25 17:51:09 +0530

    Add cascade delete and employee management

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -12,7 +12,7 @@ export const createEmp = async(req, res) => {
         if(!name || !email || !role || !designation){
             return res.status(400).json({
                 message: "All fields are required"
-            })
+            });
         }
 
         const user = await prisma.user.findUnique({
@@ -41,14 +41,14 @@ export const createEmp = async(req, res) => {
                 invitationExpiry: invitationExpiry,
                 isActive: false
             }
-        })
+        });
 
         await sendInvitationEmail(email,invitationLink);
 
         return res.status(201).json({
             message: "Employee created succesfully",
             employee
-        })
+        });
     }
 
     catch(error){
@@ -56,6 +56,105 @@ export const createEmp = async(req, res) => {
 
         return res.status(500).json({
             message: "Internal Server Error"
-        })
+        });
     }
+}
+
+export const deleteEmployee = async(req,res) => {
+
+    const id = Number(req.params.id);
+    
+    try {
+        
+        const deleteEmployee = await prisma.user.delete({
+            where: {
+                id
+            }
+        });
+
+        if(!deleteEmployee) {
+            return res.status(404).json({
+                message: "Employee not found"
+            });
+        }
+
+        if(deleteEmployee.role == "ADMIN") {
+            return res.status(403).json({
+                message: "Only employees can be deleted"
+            })
+        }
+
+        return res.status(200).json({
+            message: "Employee removed successfully",
+            deleteEmployee
+        });
+
+    } catch (error) {
+        
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
+
+export const viewAllExpenses = async(req,res) => {
+
+    try {
+        const expenses = await prisma.expense.findMany({
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        role: true
+                    }
+                }
+            }
+        });
+        return res.status(200).json({
+            expenses
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
+
+export const viewExpenseById = async(req,res) => {
+
+    const id = Number(req.params.id);
+
+    try {
+        
+        const expenses = await prisma.expense.findMany({
+            where: {
+                userId: id
+            }
+        });
+
+        if(expenses.length === 0) {
+            return res.status(404).json({
+                message: "No expenses found for this user"
+            });
+        }
+
+        return res.status(200).json({
+            expenses
+        });
+
+    } catch (error) {
+        
+        console.log(error);
+
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+
 }
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,12 +1,14 @@
 import express from "express"
-import {createEmp} from "../controllers/adminController.js"
+import {createEmp, deleteEmployee, viewAllExpenses, viewExpenseById} from "../controllers/adminController.js"
 import { authenticate } from "../middlewares/authMiddleware.js";
 import { isAdmin } from '../middlewares/adminMiddleware.js'
 
 const router = express.Router();
 
-router.post("/create-employee",authenticate,isAdmin,createEmp);
-
+router.post("/create-employee",authenticate,isAdmin,createEmp); //employee creation route
+router.delete('/:id/delete',authenticate,isAdmin,deleteEmployee); //Delete employee by id
+router.get('/expenses',authenticate,isAdmin,viewAllExpenses);  //View all expense route
+router.get('/:id/expenses',authenticate,isAdmin,viewExpenseById); // View expenses by Id
 
 
 export default router;
