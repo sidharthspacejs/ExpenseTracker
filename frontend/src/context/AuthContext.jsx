a3db01595bd93c6dce7ecc96d9ff6d9afaf34dce
sidharthspacejs <sidharthjs57@gmail.com>
2026-07-17 23:35:47 +0530

    architectural change

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,10 +1,15 @@
 import { Children, context } from "react";
 import { useState, createContext } from "react";
+import { login as loginApi } from "../api/authApi";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  const login = async (username, password) => {
+    const response = await loginApi(username, password);
+  };
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
