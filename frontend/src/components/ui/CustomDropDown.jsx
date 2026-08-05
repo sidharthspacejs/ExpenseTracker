09d9553ee705e481892477d72124d4f45426a8b9
sidharthspacejs <sidharthjs57@gmail.com>
2026-08-06 01:30:15 +0530

    CustomDropDown Ui completed

--- frontend/src/components/ui/CustomDropDown.jsx
+++ frontend/src/components/ui/CustomDropDown.jsx
@@ -1,3 +1,4 @@
+import { ChevronDown } from "lucide-react";
 import React from "react";
 import { useState } from "react";
 
@@ -5,7 +6,22 @@ const CustomDropDown = ({ icon, options, value, onChange, placeholder }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="relative">
-      <button className="flex items-center justify-between gap-3 w-40 h-12 rounded-xl border border-gray-300 bg-white px-4 shadow-sm hover:border-indigo-400 transition"></button>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center justify-between gap-3 w-40 h-12 rounded-xl border border-gray-300 bg-white px-4 shadow-sm hover:border-indigo-400 transition"
+      >
+        <div className="flex items-center gap-3">
+          {icon}
+          <span className="text-sm font-medium text-gray-700">{value}</span>
+        </div>
+        <ChevronDown
+          size={18}
+          className={`transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
+        />
+      </button>
+      {isOpen && (
+        <div className="absolute top-full left-0 mt-2 w-full h-20 bg-white border border-gray-200 rounded-xl shadow-lg overflow-hidden z-50"></div>
+      )}
     </div>
   );
 };
